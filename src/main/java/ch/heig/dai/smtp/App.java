0c20d71f05af3361ef7e8e3abd9e75d93051387a
AdrianRogner <adrian.rogner@heig-vd.ch>
2023-12-05 18:26:56 +0100

    main prends en argument les infos nécessaires

--- src/main/java/ch/heig/dai/smtp/App.java
+++ src/main/java/ch/heig/dai/smtp/App.java
@@ -4,11 +4,17 @@ import ch.heig.dai.smtp.network.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputFilter;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException {
-        if(args.length != 3){
+        String serverAddress = "localhost";
+        int serverPort = 1025;
+        if(args.length == 5){
+            serverAddress = args[3];
+            serverPort = Integer.parseInt(args[4]);
+        } else if(args.length != 3){
             throw new IOException("Pas le bom nombre d'argument, il en faut 3 :" +
                     " Liste des victims, liste des messages et nombre de groupe");
         }
@@ -16,8 +22,40 @@ public class App
         File victims = new File(args[0]);
         File messages = new File(args[1]);
         int nbrGroupe = Integer.parseInt(args[2]);
+        Configuration config = new Configuration(victims, messages, nbrGroupe);
+        String[] strVictims = config.readVictims();
+        String[] strMessages = config.readMessages();
 
-        SmtpClient client = new SmtpClient();
-        client.sendEmail();
+        if(!config.validateEmail(strVictims)){
+            throw new IOException("Email invalide");
+        }
+
+        String[][] groupeMail = config.formGroup(strVictims);
+
+        //Test
+        for (String strVictim : strVictims) {
+            System.out.println(strVictim);
+        }
+        System.out.println();
+
+        for(int i = 0; i < groupeMail.length; ++i){
+            int j = 0;
+            System.out.println("Groupe : " + (i + 1));
+            while(j < 5){
+                if(groupeMail[i][j] == null){
+                    break;
+                }
+                System.out.println(groupeMail[i][j]);
+                j++;
+            }
+        }
+        System.out.println();
+
+        for (String strMessage : strMessages) {
+            System.out.println(strMessage);
+        }
+
+        //SmtpClient client = new SmtpClient();
+        //client.sendEmail();
     }
 }
