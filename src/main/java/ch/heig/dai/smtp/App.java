17eacd13ceae7e0ccd38d521165f7331a3421dad
AdrianRogner <adrian.rogner@heig-vd.ch>
2023-12-05 17:49:31 +0100

    Updating main

--- src/main/java/ch/heig/dai/smtp/App.java
+++ src/main/java/ch/heig/dai/smtp/App.java
@@ -1,10 +1,22 @@
 package ch.heig.dai.smtp;
+import ch.heig.dai.smtp.config.Configuration;
 import ch.heig.dai.smtp.network.*;
+
+import java.io.File;
 import java.io.IOException;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException {
+        if(args.length != 3){
+            throw new IOException("Pas le bom nombre d'argument, il en faut 3 :" +
+                    " Liste des victims, liste des messages et nombre de groupe");
+        }
+
+        File victims = new File(args[0]);
+        File messages = new File(args[1]);
+        int nbrGroupe = Integer.parseInt(args[2]);
+
         SmtpClient client = new SmtpClient();
         client.sendEmail();
     }
