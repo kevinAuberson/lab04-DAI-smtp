4fbc4273c022b53ef1b658e73c945730acbc43a2
kevin <kevin.auberson@heig-vd.ch>
2023-12-06 13:50:12 +0100

    create groupe class with sender, reciever and message

--- src/main/java/ch/heig/dai/smtp/model/Group.java
+++ src/main/java/ch/heig/dai/smtp/model/Group.java
@@ -1,36 +1,41 @@
 package ch.heig.dai.smtp.model;
 
-import java.util.Random;
-/**
- * Group class represents formation of groups.
- * It creates groups of email and verify every group have at lease to email
- * @author Kevin Auberson, Adrian Rogner
- */
 public class Group {
-    /**
-     * Fonction qui permet de former les groupes de mail avec un nombre entre 2-5 mail pour chaque groupe
-     * @param group
-     * @return Un tableau de tableau de groupe de mail
-     */
-    public String[][] formGroup(int numberOfGroups, String[] group){
+    private String sender;
+    private String[] recipients;
 
-        String[][] str = new String[numberOfGroups][5];//5 est le nombre max de personne dans un groupe
-        int peopleInserted = 0;
-        for(int i = 0; i < numberOfGroups; i++){
-            //Génération d'un nombre entre 2 - 5 pour chaque groupe
-            Random rand = new Random();
-            int n = rand.nextInt(4);
-            n += 2;
-            for(int j = 0; j < n; j++){
-                if(group.length <= peopleInserted){
-                    return str;
-                }
-                str[i][j] = group[peopleInserted++];
-            }
-            if(str[i].length < 2){
-                //Insertion du premier dans la liste dans un groupe avec un seul individu (dernier groupe)
-                str[i][1] = group[0]; }
-        }
-        return str;
+    private Message message;
+
+    public Group(String sender, String[] recipients, Message message){
+        this.sender = sender;
+        this.recipients = recipients;
+        this.message = message;
+    }
+
+    /**
+     * Returns the sender of the group.
+     *
+     * @return The sender of the group.
+     */
+    public String getSender(){
+        return this.sender;
+    }
+
+    /**
+     * Returns the recipients of the group.
+     *
+     * @return The recipients of the group.
+     */
+    public String[] getRecipients(){
+        return this.recipients;
+    }
+
+    /**
+     * Returns the message of the group.
+     *
+     * @return The message of the group.
+     */
+    public Message getMessage(){
+        return this.message;
     }
 }
